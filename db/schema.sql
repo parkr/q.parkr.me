e05fda340e4c2fc166b32d70a1bf0f630f1c81ac
Parker Moore <parkrmoore@gmail.com>
2013-08-31 11:24:14 -0500

    Add the beginnings of a schema?

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `parkrm04_urls`.`Images` (
+  `uid` int(11) NOT NULL auto_increment,
+  `user` text NOT NULL,
+  `id` text NOT NULL,
+  `name` text NOT NULL,
+  `mime` text NOT NULL,
+  PRIMARY KEY  (`uid`)
+)
