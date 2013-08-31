906769dadd987d8872b946bd2a1a88161c1b071b
Parker Moore <parkrmoore@gmail.com>
2013-08-31 11:04:10 -0500

    Upload form.

---
+++ upload.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Upload: Q.Parkr.Me</title>
+<link href="qparkrme.css" rel="stylesheet" media="all" />
+</head>
+
+<body>
+<div id="container" style="width:600px;text-align:center;">
+  <div id="header" style="display:block;">Upload: Q.Parkr.Me</div>
+  <form action="/u/" method="post" enctype="multipart/form-data" name="form1" id="form1">
+    <p>
+      <label>Username:
+        <input type="text" name="username" id="username" />
+      </label>
+      <br />
+      <input type="file" name="media" id="media" />
+      <br />
+      <input type="submit" name="submit" id="submit" value="Submit" />
+    </p>
+  </form>
+</div>
+</body>
+</html>
